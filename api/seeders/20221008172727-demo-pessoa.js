e8790d566e2dd8124cf48b09256a223c5a58e953
sossup <davibarguarti@gmail.com>
2022-10-08 20:16:19 -0300

    adicionando mais semente

--- api/seeders/20221008172727-demo-pessoa.js
+++ api/seeders/20221008172727-demo-pessoa.js
@@ -54,6 +54,6 @@ module.exports = {
   },
 
   async down (queryInterface, Sequelize) {
-    await queryInterface.bulkDelete('People', null, {});
+    return queryInterface.bulkDelete('Pessoas', null, {})
   }
 };
